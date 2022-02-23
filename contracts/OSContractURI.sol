eda36cada9853660e4f9abb1e94f711f08fc7141
Evolve <Artevolve@yahoo.com>
2022-02-23 22:15:03 +0100

    feat: init 👏
    - Base Smart Contract ERC721 - Exposure Collection
    next: dutch auction/random mint

---
+++ contracts/OSContractURI.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+abstract contract OSContractURI {
+    string internal _contractURI;
+
+    function contractURI() public view returns (string memory) {
+        return _contractURI;
+    }
+
+    function setContractURI(string calldata uri) public virtual {
+        _contractURI = uri;
+    }
+}
